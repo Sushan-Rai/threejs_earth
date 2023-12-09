09ced5369eed905eb797be1b268ff808bcc0bb38
Sushan-Rai <131355216+Sushan-Rai@users.noreply.github.com>
2023-12-09 12:17:33 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,9 @@
 import vitePluginString from 'vite-plugin-string'
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
 
 export default {
   plugins: [
-    vitePluginString(),
-    react()
+    vitePluginString()
   ],
   base: '/threejs_earth/'
 }
